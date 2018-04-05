1f5ae13318ee18368e7bb0eb501de39c62dc107f
ali-tit <alibenali.ing@gmail.com>
2018-04-05 14:19:47 +0200

    symfony starter

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,15 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+      $securityContext = $this->container->get('security.authorization_checker');
+      if ($securityContext->isGranted('ROLE_USER')) {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ));
+      }else {
+        return $this->redirectToRoute('fos_user_security_login', array(), 301);
+      }
+
     }
 }
